8d1a3e888c54dfd4cc50e492cc6e56e75987503a
Jovani Pink <jovanirosado@me.com>
2020-03-21 12:11:53 -0400

    logo changes

--- src/@narative/gatsby-theme-novela/components/Logo/Logo.tsx
+++ src/@narative/gatsby-theme-novela/components/Logo/Logo.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-import mediaqueries from "@styles/media";
-import customeLogo from '../../../../assets/logos/ms-logo.svg'
+import mediaQueries from "@styles/media";
+import customLogo from '../../../../assets/logos/jovi-logo.svg'
 
 const Logo = ({ fill = "#fff" }: { fill?: string }) => {
   const _fill = fill === '#000' ? null : fill
   return (
     <LogoContainer>
-      <img src={customeLogo} style={{height:40, width:40, padding:1, backgroundColor: _fill}} />
+      <img src={customLogo} style={{height:40, width:40, padding:1, backgroundColor: _fill}} />
     </LogoContainer>
   );
 };
@@ -19,11 +19,11 @@ const LogoContainer = styled.div`
   .Logo__Mobile {
     display: none;
   }
-  ${mediaqueries.tablet`
+  ${mediaQueries.tablet`
     .Logo__Desktop {
       display: none;
     }
-    
+
     .Logo__Mobile{
       display: block;
     }
