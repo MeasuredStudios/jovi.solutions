14cd2d299ea8b80ca9e8d3a4971659762201c011
Jovani Pink <jovanirosado@me.com>
2020-03-21 17:11:55 -0400

    fixed the logo

--- src/@narative/gatsby-theme-novela/components/Logo/Logo.tsx
+++ src/@narative/gatsby-theme-novela/components/Logo/Logo.tsx
@@ -1,19 +1,25 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 
-import mediaQueries from "@styles/media";
-import customLogo from '../../../../assets/logos/jovi-logo.svg'
+import mediaQueries from '@styles/media';
 
-const Logo = ({ fill = "#fff" }: { fill?: string }) => {
-  const _fill = fill === '#000' ? null : fill
+const Logo = ({ fill = '#fff' }: { fill?: string }) => {
+  const _fill = fill === '#000' ? null : fill;
   return (
     <LogoContainer>
-      <img src={customLogo} style={{height:40, width:40, padding:1, backgroundColor: _fill}} />
+      <BrandText>JOVI</BrandText>
     </LogoContainer>
   );
 };
 
 export default Logo;
+
+const commonStyles = (p) => css`
+  font-weight: bold;
+  color: ${p.theme.colors.primary};
+  font-family: ${p.theme.fonts.sansserif};
+`;
 
 const LogoContainer = styled.div`
   .Logo__Mobile {
@@ -28,4 +34,13 @@ const LogoContainer = styled.div`
       display: block;
     }
   `}
+`;
+
+const BrandText = styled.h4`
+  font-size: 1 rem;
+  word-break: keep-all;
+  font-size: 18px;
+  line-height: 1.45;
+  font-weight: bold;
+  ${commonStyles};
 `;
